07f3adceac9ea67a11f5670de01388802494943c
Lei-DONG-TW <ldong@thoughtworks.com>
2018-06-30 17:03:55 +0800

    Add Seed Data for database

---
+++ Migrations/20180630085005_SeedData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZhenFa.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO \"Orders\" (\"Customer\", \"OrderDate\") VALUES ('测试', '2018-01-01');");
+
+            var selectOrderId = "(SELECT \"Id\" FROM \"Orders\" WHERE \"Customer\" = '测试')";
+
+            migrationBuilder.Sql($"INSERT INTO \"Items\" (\"Name\", \"Count\", \"Specs\", \"OrderId\") VALUES ('测试-Item-1', '1', '规格-1', {selectOrderId});");
+            migrationBuilder.Sql($"INSERT INTO \"Items\" (\"Name\", \"Count\", \"Specs\", \"OrderId\") VALUES ('测试-Item-2', '2', '规格-1', {selectOrderId});");
+            migrationBuilder.Sql($"INSERT INTO \"Items\" (\"Name\", \"Count\", \"Specs\", \"OrderId\") VALUES ('测试-Item-3', '3', '规格-1', {selectOrderId});");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"Orders\"");
+        }
+    }
+}
