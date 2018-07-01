007d2f74942162804f96b027121c062e36fcb7d4
Lei-DONG-TW <ldong@thoughtworks.com>
2018-07-01 16:12:39 +0800

    add a new controller, but has self reference loop issue

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZhenFa.Models;
+using ZhenFa.Persistence;
+
+namespace ZhenFa.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ZhenFaDbContext _context;
+
+        public OrderController(ZhenFaDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("api/orders")]
+        public async Task<IEnumerable<Order>> Index()
+        {
+            return await _context.Orders.Include(o => o.Items).ToListAsync();
+        }
+    }
+}
--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -20,7 +21,7 @@ namespace ZhenFa.Models
 
         public Order()
         {
-            Items = new List<Item>();
+            Items = new Collection<Item>();
         }
     }
 }
